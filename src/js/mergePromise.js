6ac546788bb5e7f98907217c92694d605fd8ac9a
JackFGreen <clcortana@163.com>
2018-08-30 22:52:42 +0800

    md mergePromise

--- src/js/mergePromise.js
+++ src/js/mergePromise.js
@@ -19,56 +19,64 @@ const ajax3 = () => timeout(2000).then(() => {
 
 const mergePromise = ajaxArray => {
   // 1,2,3 done [1,2,3] es6 + es3
-  function handle (callback) {
-    const arr = []
-    let index = 0
-    const len = ajaxArray.length
+  // function handle (callback) {
+  //   const arr = []
+  //   let index = 0
+  //   const len = ajaxArray.length
 
-    function run () {
-      ajaxArray[index]().then(r => {
-        arr.push(r)
-        if (index < len - 1) {
-          index++
-          run()
-        } else {
-          callback(arr)
-        }
-      })
+  //   function run () {
+  //     ajaxArray[index]().then(r => {
+  //       arr.push(r)
+  //       if (index < len - 1) {
+  //         index++
+  //         run()
+  //       } else {
+  //         callback(arr)
+  //       }
+  //     })
+  //   }
+  //   run()
+  // }
+
+  // function P (fn) {
+  //   const callbacks = []
+
+  //   this.then = function (callback) {
+  //     callbacks.push(callback)
+  //   }
+
+  //   function resolve (res) {
+  //     setTimeout(() => {
+  //       for (let i = 0; i < callbacks.length; i++) {
+  //         const fn = callbacks[i]
+  //         fn(res)
+  //       }
+  //     }, 0)
+  //   }
+
+  //   typeof fn === 'function' && fn(resolve)
+  // }
+
+  // if (typeof Promise === 'function') {
+  //   return new Promise((resolve, reject) => {
+  //     handle(resolve)
+  //   })
+  // } else {
+  //   return new P((resolve, reject) => {
+  //     handle(resolve)
+  //   })
+  // }
+  const result = []
+  async function asyncEach () {
+    for (const ajax of ajaxArray) {
+      result.push(await ajax())
     }
-    run()
+    return result
   }
-
-  function P (fn) {
-    const callbacks = []
-
-    this.then = function (callback) {
-      callbacks.push(callback)
-    }
-
-    function resolve (res) {
-      setTimeout(() => {
-        for (let i = 0; i < callbacks.length; i++) {
-          const fn = callbacks[i]
-          fn(res)
-        }
-      }, 0)
-    }
-
-    typeof fn === 'function' && fn(resolve)
-  }
-
-  if (typeof Promise === 'function') {
-    return new Promise((resolve, reject) => {
-      handle(resolve)
-    })
-  } else {
-    return new P((resolve, reject) => {
-      handle(resolve)
-    })
-  }
+  return asyncEach()
 }
 
-mergePromise([ajax1, ajax2, ajax3])
+const a = mergePromise([ajax1, ajax2, ajax3])
   .then(function (data) {
     console.log('done')
     console.log(data) // data 为 [1, 2, 3]
